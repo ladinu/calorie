b4040788eef5eb9b6f1c03b514cb5b2a41b227b1
Ladinu Chandrasinghe <ladinu@gmail.com>
2015-05-11 00:34:38 -0700

    Added ops class

--- main.hs
+++ main.hs
@@ -1,3 +1,5 @@
+module Main where
+
 data Unit = Gram Float | Ounce Float | FluidOunce Float deriving (Show, Eq)
 type Gram = Float
 
@@ -12,26 +14,34 @@ data Ingredient = Ingredient String MacroNutrients Unit deriving (Show, Eq)
 data Recipe = Recipe String [Ingredient] Int deriving (Show, Eq)
 
 
-class Macros a where
-  fats, carbs, protein :: a -> Gram
+class Ops a where
   (.*), (./) :: a -> Float -> a
   (./) m n = m .* (1/n)
 
+class Macros a where
+  fats, carbs, protein :: a -> Gram
 
 instance Macros MacroNutrients where
   fats (MacroNutrients (Fat g) _ _) = g
   carbs (MacroNutrients _ (Carbohydrate g) _) = g
   protein (MacroNutrients _ _ (Protein g)) = g
 
+instance Ops MacroNutrients where
   (.*) m n =
     MacroNutrients (Fat (fats m * n))
     (Carbohydrate (carbs m * n))
     (Protein (protein m * n))
 
--- instance Macros Ingredient where
---   fats (Ingredient _ m _) = fats m
---   carbs (Ingredient _ m _) = carbs m
---   protein (Ingredient _ m _) = protein m
---
---   (//) (Ingredient name macros amount) n =
---     Ingredient name (macros // n) (Gram 0) -- FIX
+instance Ops Unit where
+   (.*) (Gram m) n = Gram (m*n)
+   (.*) (Ounce m) n = Ounce (m*n)
+   (.*) (FluidOunce m) n = FluidOunce (m*n)
+
+instance Macros Ingredient where
+  fats (Ingredient _ m _) = fats m
+  carbs (Ingredient _ m _) = carbs m
+  protein (Ingredient _ m _) = protein m
+
+instance Ops Ingredient where
+  (.*) (Ingredient name macros amount) n =
+    Ingredient name (macros .* n) (amount .* n) -- FIX
