304bc3ef8661b0033437e69fd8f6ba8742f7c47c
Ladinu Chandrasinghe <ladinu@gmail.com>
2015-05-12 21:11:20 -0700

    Updated mealplan

--- mealplan.hs
+++ mealplan.hs
@@ -1,22 +1,63 @@
 import Macros
 
 -- All the ingredients
-cornstarch = ingredient "Corn Starch" 0 7 0 (TableSpoon 1)
-oil = ingredient "Oil" 14 0 0 (TableSpoon 1)
-chicken = ingredient "Chicken" 8.4 0 157.1 (Pound 1.5)
-onion = ingredient "Onion" 0.1 17.2 1.6 (Gram 170)
-pepper = ingredient "Red Bell Pepper" 0.6 12.6 2 (Gram 200)
-garlic = ingredient "Garlic" 0 5 1 (Gram 15)
 yogurt = ingredient "Fage Greek Yogurt" 0 9 23 (Gram 227)
 eggwhites = ingredient "Egg whites" 0 0 20 (Gram 160)
 ricemilk = ingredient "Rice Milk" 2.5 23 1 (Cup 1)
 proteinpowder = ingredient "Whey protein" 1 3 24 (Gram 30)
-potato = ingredient "Potato" 0.1 27 2.3 (Gram 150)
-rice = ingredient "Medium Grain White Rice, cooked" 0.2 28.6 2.4 (Gram 100)
 sugar = ingredient "White Sugar" 0 16 0 (Gram 16)
+rice = ingredient "Medium Grain White Rice, cooked" 0.2 28.6 2.4 (Gram 100)
+
+potato   = ingredient "Potato" 0.1 27 2.3 (Gram 150)
+mushroom = ingredient "Mushroom" 0 3 1 (Gram 85)
+beans    = ingredient "Black Beans (drained)" 0.5 22 7 (Gram 130)
+tomato   = ingredient "Diced Tomato - Chili Ready" 0 7 1 (Gram 121)
+turkey   = ingredient "Ground Turkey - 93% lean" 8 0 22 (Gram 112)
+chicken  = ingredient "Chicken" 1.5 0 26 (Ounce 4)
+oil      = ingredient "Oil" 14 0 0 (TableSpoon 1)
+garlic   = ingredient "Garlic" 0 5 1 (Gram 15)
+pepper   = ingredient "Red Bell Pepper" 0.6 12.6 2 (Gram 200)
+onion    = ingredient "Onion" 0.1 17.2 1.6 (Gram 170)
+ginger   = ingredient "Ginger" 0.1 2.7 0.3 (Gram 15)
+starch   = ingredient "Corn Starch" 0 10 0 (Gram 10)
+onion'   = ingredient "Onion" 0 11.1 1 (Gram 110)
+broth    = ingredient "Chicken broth" 0 1 4 (Cup 1)
+paste    = ingredient "Tomato Paste" 0 7 1 (Gram 35)
 
 -- Recipies
-lunch = Recipe "Chicken & Basil stir-fry"
-               [chicken, cornstarch, oil, onion, pepper, garlic, (rice .* 6)]
-               6
-preworkout = Recipe "Preworkout Shake" [proteinpowder, ricemilk] 1
+chicken' = chicken .* 6.5
+rice'    = rice .* 6
+lunch    =
+  Recipe "Chicken & Basil stir-fry and Rice"
+  [chicken', oil, garlic, pepper, onion, ginger, starch, rice']
+  6
+
+shake1 =
+  Recipe "Pre-Workout Shake"
+  [proteinpowder, ricemilk]
+  1
+
+potato'   = potato .* 6
+mushroom' = mushroom .* 3
+garlic'   = garlic
+oil'      = oil ./ 6
+beans'    = beans .* 3.5
+tomato'   = tomato .* 3.5
+turkey'   = turkey .* 5
+chili     =
+  Recipe "Chili and Potato"
+  [potato', mushroom', onion', garlic', oil', broth, beans', paste, tomato', turkey']
+  6
+
+shake2 =
+  Recipe "Post Workout shake"
+  [proteinpowder .* 2]
+  1
+
+yogurt' = yogurt ./ 2
+dessert =
+  Recipe "Dessert"
+  [yogurt', sugar]
+  1
+
+intake = [lunch, shake1, chili, shake2, dessert]
