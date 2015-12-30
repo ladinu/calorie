547e0aff012460ffc614d98d71fbae7583e018bf
Ladinu Chandrasinghe <ladinu@gmail.com>
2015-12-29 16:16:24 -0800

    Improvements

--- macros.hs
+++ macros.hs
@@ -2,113 +2,56 @@
 
 module Macros where
 
-data Unit = Gram Float |
-            Ounce Float |
-            Pound Float |
-            FluidOunce Float |
-            Cup Float |
-            TableSpoon Float|
-            TeaSpoon Float deriving (Show, Eq)
-type Gram = Float
+type NumServings  = Int
+type Gram         = Float
 
-data Fat = Fat Gram deriving (Show, Eq)
-data Carbohydrate = Carbohydrate Gram deriving (Show, Eq)
-data Protein = Protein Gram deriving (Show, Eq)
+type Fat          = Gram
+type Carbohydrate = Gram
+type Protein      = Gram
 
-data MacroNutrients = MacroNutrients Fat Carbohydrate Protein deriving (Show, Eq)
-
-data Ingredient = Ingredient String MacroNutrients Unit deriving (Show, Eq)
-data Recipe = Recipe String [Ingredient] Int deriving (Show, Eq)
+data Unit         = Unit Float String deriving (Show, Eq)
+data Food         =
+  Ingredient String Fat Carbohydrate Protein Unit |
+  Meal String NumServings [Food]  deriving (Show, Eq)
 
 
-class Show' a where
-  show' :: a -> String
+class Macros a where
+  fats, carbs, protein :: a -> Gram
+
+class Display a where
+  display :: a -> String
 
 class Ops a where
   (.*), (./) :: a -> Float -> a
   (./) m 0 = error "Cannot divide by zero"
   (./) m n = m .* (1/n)
 
-instance Ops MacroNutrients where
-  (.*) m n =
-    MacroNutrients (Fat (fats' m * n))
-    (Carbohydrate (carbs' m * n))
-    (Protein (protein' m * n))
+
+instance Macros Food where
+  fats    (Ingredient _ f _ _ _) = f
+  fats    (Meal _ servings fs) = sum (map fats fs) / fromIntegral servings
+  carbs   (Ingredient _ _ c _ _) = c
+  carbs   (Meal _ servings fs) = sum (map carbs fs) / fromIntegral servings
+  protein (Ingredient _ _ _ p _) = p
+  protein (Meal _ servings fs ) = sum (map protein fs) / fromIntegral servings
+
 
 instance Ops Unit where
-   (.*) (Gram m) n = Gram (m*n)
-   (.*) (Ounce m) n = Ounce (m*n)
-   (.*) (Pound m) n = Pound (m*n)
-   (.*) (FluidOunce m) n = FluidOunce (m*n)
-   (.*) (Cup m) n = Cup (m*n)
-   (.*) (TableSpoon m) n = TableSpoon (m*n)
-   (.*) (TeaSpoon m) n = TeaSpoon (m*n)
+   (.*) (Unit m name) n = Unit (m*n) name
 
-instance Show' Unit where
-  show' (Gram m) = show m ++ "g"
-  show' (Ounce m) = show m ++ " oz"
-  show' (Pound m) = show m ++ " lb"
-  show' (FluidOunce m) = show m ++ " flOz"
-  show' (Cup m) = show m ++ " Cup"
-  show' (TableSpoon m) = show m ++ " Tbsp"
-  show' (TeaSpoon m) = show m ++ " Tsp"
+instance Ops Food where
+  (.*) (Ingredient name f c p amount) n =
+    Ingredient name (f * n) (c * n) (p * n) (amount .* n)
 
-instance Ops Ingredient where
-  (.*) (Ingredient name macros amount) n =
-    Ingredient name (macros .* n) (amount .* n)
+instance Display Unit where
+  display (Unit amount name) = show amount ++ " " ++ name
 
-instance Show' Ingredient where
-  show' (Ingredient name _ u) = name ++ " " ++ show' u
+instance Display Food where
+  display (Ingredient name _ _ _ u) = name ++ " " ++ display u
+  display (Meal name servings fs) =
+    "------- " ++ name ++ " [" ++ show servings ++ "] -------\n"
+    ++ concat (map (++ "\n") (map display fs))
 
-instance Show' [Ingredient] where
-  show' [] = ""
-  show'(x:xs) = show' x ++ "\n" ++ show' xs
-
-class Macros a where
-  fats', carbs', protein' :: a -> Gram
-
-instance Macros MacroNutrients where
-  fats' (MacroNutrients (Fat g) _ _) = g
-  carbs' (MacroNutrients _ (Carbohydrate g) _) = g
-  protein' (MacroNutrients _ _ (Protein g)) = g
-
-instance Macros Ingredient where
-  fats' (Ingredient _ m _) = fats' m
-  carbs' (Ingredient _ m _) = carbs' m
-  protein' (Ingredient _ m _) = protein' m
-
-instance Macros [Ingredient] where
-  fats' = sum . map (fats')
-  carbs' = sum . map (carbs')
-  protein' = sum . map (protein')
-
-instance Macros Recipe where
-  fats' (Recipe _ is servings) = (fats' is) / fromIntegral servings
-  carbs' (Recipe _ is servings) = (carbs' is) / fromIntegral servings
-  protein' (Recipe _ is servings) = (protein' is) / fromIntegral servings
-
-instance Show' Recipe where
-  show' (Recipe name ingredients servingSize) =
-    "------- " ++ name ++ " [" ++ show servingSize ++ "] -------\n"
-    ++ show' ingredients
-
-instance Macros [Recipe] where
-  fats' = sum . map (fats')
-  carbs' = sum . map (carbs')
-  protein' = sum . map (protein')
-
-
-fats :: (Macros a, Integral b) => a -> b
-fats = ceiling . fats'
-
-carbs :: (Macros a, Integral b) => a -> b
-carbs = ceiling . carbs'
-
-protein :: (Macros a, Integral b) => a -> b
-protein = ceiling . protein'
-
-macros :: (Macros a, Integral b) => a -> (b, b, b)
-macros a = (fats a, carbs a, protein a)
 
 tf :: (Real a, Fractional b) => a -> b
 tf = realToFrac
@@ -116,8 +59,12 @@ tf = realToFrac
 calories :: (Macros a, Fractional b) => a -> b
 calories m = sum [tf (fats m) * 9, tf (carbs m) * 4, tf (protein m) * 4]
 
-macro :: Float -> Float -> Float -> MacroNutrients
-macro f c p = MacroNutrients (Fat f) (Carbohydrate c) (Protein p)
+macros f = (ceiling (fats f), ceiling (carbs f), ceiling (protein f))
 
-ingredient :: String -> Float -> Float -> Float -> Unit -> Ingredient
-ingredient n f c p u = Ingredient n (macro f c p) u
+toKG :: Double -> Double
+toKG = (*) 0.453592
+
+bmr :: Double -> Double -> Double
+bmr bw bf = 370.0 + (21.6 * (toKG(bw) * (1.0-bf)))
+
+prnt a = putStr $ display a ++ "\n"
---
+++ weekday.hs
@@ -0,0 +1,39 @@
+import Macros
+
+intake =
+  Meal "Weekday" 1
+  [
+     Ingredient "Whey Protein Powder" 1 3 24 (Unit 1 "Scoop"),
+     Meal "Lunch" 1
+     [
+       Ingredient "Medium Grain White Rice (Cooked)" 0.22 28.6 2.4 (Unit 100 "g"),
+       Meal "Chicken stir-fry" 6
+       [
+         Ingredient "Oil" 14 0 0 (Unit 1 "Tbsp"),
+         (Ingredient "Chicken" 1.5 0 26 (Unit 4 "oz")) .* 6.5,
+         Ingredient "Bell Pepper" 0.6 12.6 2 (Unit 200 "g"),
+         Ingredient "Onion" 0 11.1 1 (Unit 110 "g")
+       ],
+       Meal "Shrimp stir-fry" 6
+       [
+         (Ingredient "Oil" 14 0 0 (Unit 1 "Tbsp")) .* 2,
+         Ingredient "Onion" 0.1 17.2 1.6 (Unit 170 "g"),
+         Ingredient "Brussel Sprouts" 0.7 20.3 7.7 (Unit 8 "oz"),
+         Ingredient "Shrimp" 11.8 6.2 138.7 (Unit 1.5 "lb")
+       ]
+     ],
+     Ingredient "Whey+ Protein Powder" 0 1 22 (Unit 1 "Scoop"),
+     Ingredient "Rice Milk" 2.5 23 1 (Unit 1 "Cup"),
+     Meal "Chicken Enchilada Rice Casserolse" 6
+     [
+       Ingredient "White Rice (Raw)" 2.4 295.8 26.4 (Unit 2 "Cup"),
+       Ingredient "Chicken Breast (Raw)" 11.2 0 209.5 (Unit 2 "lb"),
+       (Ingredient "Enchilada Sauce" 0 4 0 (Unit 0.25 "Cup")) .* 13,
+       (Ingredient "Refried Beans" 0 16 6 (Unit 0.5 "Cup")) .* 3.5,
+       Ingredient "Corn kernels" 0 18 0 (Unit 90 "g"),
+       (Ingredient "Cheese" 9 1 7 (Unit 28 "g")) .* 8,
+       Ingredient "Black Beans (drained)" 0.5 22 7 (Unit 130 "g"),
+       Ingredient "Onion" 0 11.1 1 (Unit 110 "g")
+     ]
+    --  Ingredient "PB2" 1 6 5 (Unit 12 "g")
+  ]
